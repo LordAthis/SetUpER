ce0a8c4516d0ee6b16b21123e1dd88ddaf3c1638
LordAthis <lordathis@gmail.com>
2026-04-17 02:28:42 +0200

    Modify Install-TC.ps1 for installation process
    Updated Install-TC.ps1 to include parameters for installation directory and added logic to check for TC.exe.

---
+++ Script/Install-TC.ps1
@@ -0,0 +1,9 @@
+param([string]$InstallDir = "C:\Program Files")
+
+$appPath = "..\Apps\TC.exe"
+if(Test-Path $appPath) {
+    Start-Process -FilePath $appPath -ArgumentList "/S" -Wait -NoNewWindow
+    Write-Output "Total Commander telepítve"
+} else {
+    Write-Error "TC.exe nem található!"
+}
