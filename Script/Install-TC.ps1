a42b374c68768438a94b6ee4d7bc80731cab8fb2
LordAthis <lordathis@gmail.com>
2026-04-17 03:27:07 +0200

    Add comment for Total Commander installation
    Added comment for Total Commander installation process.

--- Script/Install-TC.ps1
+++ Script/Install-TC.ps1
@@ -2,6 +2,7 @@ param([string]$InstallDir = "C:\Program Files")
 
 $appPath = "../Apps/TC.exe"
 if(Test-Path $appPath) {
+    # Total Commander csendes telepítés
     Start-Process -FilePath $appPath -ArgumentList "/S" -Wait -NoNewWindow
     Write-Output "Total Commander telepítve"
 } else {
