f85e72e50f0ad293b28e6bb36dfbd5a18f9e5f1c
LordAthis <lordathis@gmail.com>
2026-04-17 02:41:55 +0200

    Fix path separator in Install-TC.ps1

--- Script/Install-TC.ps1
+++ Script/Install-TC.ps1
@@ -1,6 +1,6 @@
 param([string]$InstallDir = "C:\Program Files")
 
-$appPath = "..\Apps\TC.exe"
+$appPath = "../Apps/TC.exe"
 if(Test-Path $appPath) {
     Start-Process -FilePath $appPath -ArgumentList "/S" -Wait -NoNewWindow
     Write-Output "Total Commander telepítve"
