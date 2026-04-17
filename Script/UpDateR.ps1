29f31e02cbe32671439c2078ef3cf6ea14012c69
LordAthis <lordathis@gmail.com>
2026-04-17 02:41:31 +0200

    Update file paths for configuration and apps list

--- Script/UpDateR.ps1
+++ Script/UpDateR.ps1
@@ -1,11 +1,11 @@
 param([string]$AppId)
 
-$config = Get-Content "../Config.json" | ConvertFrom-Json
-$appsList = Get-Content "../AppsList.json" | ConvertFrom-Json
+$config = Get-Content "Config.json" | ConvertFrom-Json
+$appsList = Get-Content "../Apps/AppsList.json" | ConvertFrom-Json
 
 $app = $appsList.apps | Where-Object { $_.id -eq $AppId }
 if($app) {
-    $appPath = "..\Apps\$($app.id).exe"
+    $appPath = "$($config.downloadDir)\$($app.id).exe"
     if(-not (Test-Path $appPath) -or ((Get-Item $appPath).Length -lt 1MB)) {
         Write-Output "Letöltés: $($app.name)"
         Invoke-WebRequest -Uri $app.downloadUrl -OutFile $appPath -UseBasicParsing
